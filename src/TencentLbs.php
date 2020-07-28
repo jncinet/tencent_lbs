a32ec0c6d61eccb8eff2556281562f41ccf80eb5
张晔 <y@qilephp.com>
2020-07-28 11:09:47 +0800

    add sign

--- src/TencentLbs.php
+++ src/TencentLbs.php
@@ -7,15 +7,44 @@ use Illuminate\Support\Facades\Cache;
 
 class TencentLbs
 {
-    protected function get($url = '', $query = [])
+    /**
+     * 格式化请求参数生成签名
+     *
+     * @param string $path 请求路径
+     * @param array $query 请求参数
+     * @return array
+     */
+    protected function formatQueryString($path, $query)
     {
-        $client = new Client(['verify' => false]);
-        $response = $client->request(
-            'GET',
-            $url,
-            [
-                'query' => $query
-            ]);
+        $query = array_merge(['key' => Cache::get('config_map_tencent_lbs_key', '')], $query);
+        sort($query);
+
+        $sign = md5($path . '?' . http_build_query($query) . Cache::get('config_map_tencent_lbs_sk', ''));
+        return array_merge(['sig' => $sign], $query);
+    }
+
+    /**
+     * 发送请求
+     *
+     * @param string $path
+     * @param array $query
+     * @param string $method
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function request($path = '', $query = [], $method = 'GET')
+    {
+        $query = $this->formatQueryString($path, $query);
+
+        if ($method === 'GET') {
+            $query = ['query' => $query];
+        } else {
+            $query = ['body' => $query];
+        }
+
+        $client = new Client(['base_uri' => 'https://apis.map.qq.com', 'verify' => false]);
+        $response = $client->request($method, $path, $query);
+
         return json_decode((string)$response->getBody(), true);
     }
 
@@ -24,16 +53,11 @@ class TencentLbs
      *
      * @param $ip
      * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function ipLocation($ip)
     {
-        return $this->get(
-            'https://apis.map.qq.com/ws/location/v1/ip',
-            [
-                'ip' => $ip,
-                'key' => Cache::get('config_map_tencent_lbs_key', '')
-            ]
-        );
+        return $this->request('/ws/location/v1/ip', ['ip' => $ip]);
     }
 
     /**
@@ -42,15 +66,10 @@ class TencentLbs
      * @param string $latitude
      * @param string $longitude
      * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function gpsLocation($latitude = '0', $longitude = '0')
     {
-        return $this->get(
-            'https://apis.map.qq.com/ws/geocoder/v1/',
-            [
-                'location' => $latitude . ',' . $longitude,
-                'key' => Cache::get('config_map_tencent_lbs_key', '')
-            ]
-        );
+        return $this->request('/ws/geocoder/v1', ['location' => $latitude . ',' . $longitude]);
     }
 }
